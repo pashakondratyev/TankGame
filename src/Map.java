45af5c85000f1e45a51c79fd8664f6c8b92880f6
Inan42 <inantwain@gmail.com>
2015-06-13 19:53:09 -0400

    Update Map.java

--- src/Map.java
+++ src/Map.java
@@ -3,5 +3,21 @@ import java.util.*;
 
 public class Map {
     int x,y;
+    char[][] map;
+    
+    try {	    
+	    Scanner sc = new Scanner(new File("map.dat"));
+	    int j=0;
+
+	    while (sc.hasNext()) {
+		String line = sc.nextLine();
+		for (int i=0; i < maxX; i++) {
+		    map[i][j] = line.charAt(i);
+		}
+		j++;
+	    }
+	}
+	catch (Exception e) {}	
+    }
 
 }
