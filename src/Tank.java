9e3e877da5f4fb945019fc37050f24c769af0dcb
Inan42 <inantwain@gmail.com>
2015-06-14 19:09:47 -0400

    Update Tank.java

--- src/Tank.java
+++ src/Tank.java
@@ -9,19 +9,20 @@ import java.lang.Math;
 
 
 public class Tank {
-    double x = 100;
+    double x = 0;
     double xa = 0;
     double y = 330;
     double ya = 0;
-    double theta = 0;
-    double speed = .5;
+    double a = 0;
+    int tmpangle = 0;
     private Game game;
     private BufferedImage sprite, missile;
+    boolean moveforward, movebackward;
 
     public Tank(Game game) {
         this.game= game;
         try{
-            sprite = ImageIO.read(new File("Assets" + File.separator + "PlayerOne.png"));
+            sprite = ImageIO.read(new File(""));
         }
         catch (Exception e){
             e.printStackTrace();
@@ -29,63 +30,79 @@ public class Tank {
 
     }
 
-    public void update() {
-        if (x + xa > 0 && x + xa < game.getWidth()-30)
-            x = x + xa * speed;
-        if (y + ya > 0 && y + ya < game.getHeight()-60)
-            y = y + ya * speed;
+    public void setA(int angle){
+        a = Math.toRadians(angle);
     }
 
+    public void move() {
+        if (tmpangle > 360)
+            tmpangle = 0;
+        else if (tmpangle < 0)
+            tmpangle = 360;
+
+        setA(tmpangle);
+
+        if (moveforward)
+            moveforward();
+        else if (movebackward)
+            movebackward();
+        System.out.println(tmpangle);
+    }
+
+
+    public void moveforward() {
+        x += Math.sin(a);
+        y += Math.cos(a);
+    }
+
+    public void movebackward() {
+        x -= Math.sin(a);
+        y -= Math.cos(a);
+    }
     public void draw(Graphics2D g) {
-        g.drawImage( rotate(theta, sprite), (int)Math.round(x), (int)Math.round(y), null);
+        rotate (a, sprite);
+        g.drawImage(sprite, (int)Math.round(x), (int)Math.round(y), null);
     }
 
     public BufferedImage rotate( double degree , BufferedImage i) {
         double rotationRequired = Math.toRadians(degree);
-        double locationX = i.getWidth() / 2;
-        double locationY = i.getHeight() / 2;
+        double locationX = i.getWidth() / 1.5;
+        double locationY = i.getHeight() / 1.5;
         AffineTransform tx = new AffineTransform();
 
-        tx.scale(.5,.5);
+        tx.scale(.7,.7);
         tx.rotate(rotationRequired, locationX, locationY);
         AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
         return op.filter(i, null);
     }
     public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_RIGHT)
-            keyPressed(e);
-        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_DOWN)
-            ya = 0;
-            xa = 0;
+        if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            tmpangle += 0;
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            tmpangle += 0;
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+            moveforward = false;
+        if (e.getKeyCode() == KeyEvent.VK_DOWN)
+            movebackward = false;
+
     }
 
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_LEFT)
-            theta -= 15;
+            tmpangle -= 10;
         if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-            theta += 15;
+            tmpangle += 10;
         if (e.getKeyCode() == KeyEvent.VK_UP)
-            ya = -Math.cos(Math.toRadians(theta));
-            xa = -Math.sin(Math.toRadians(theta));
+            moveforward = true;
         if (e.getKeyCode() == KeyEvent.VK_DOWN)
-            ya = Math.cos(Math.toRadians(theta));
-            xa = Math.sin(Math.toRadians(theta));
+           movebackward = true;
     }
 }
-
-
 /*
-if(angle < 90){
-dx = Math.cos(angle);
-dy = -Math.sin(angle)*dpm;
-}else if(angle < 180){
-dx = -Math.sin(angle-90)*dpm;
-dy = -Math.cos(angle-90)*dpm;
-}else if(angle < 270){
-dx = -Math.cos(angle-180)*dpm;
-dy = Math.sin(angle-180)*dpm;
-}else{
-dx = Math.sin(angle-270)*dpm;
-dx = Math.cos(angle-270)*dpm;
+direction.x = (float) Math.cos(Math.toRadians(rotation));
+direction.y = (float) Math.sin(Math.toRadians(rotation));
+if (direction.length() > 0) {
+    direction = direction.normalise();
 }
  */
+
